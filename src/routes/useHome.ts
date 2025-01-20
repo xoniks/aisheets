53fd5e83cef74075d6755c39f6276fd5bf926512
Damián Pumar <damianpumar@gmail.com>
2025-01-20 13:51:21 +0100

    feat: :zap: Improve

--- src/routes/useHome.ts
+++ src/routes/useHome.ts
@@ -5,72 +5,69 @@ import { useRows, type Column, useColumns, type Row } from "~/state";
 const responses = [
   {
     rowId: "1",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi .",
   },
   {
     rowId: "2",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dit anim id est laborum.",
   },
   {
     rowId: "3",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "4",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliqtat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "5",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "6",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "7",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "8",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et doloreat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "9",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "10",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquiccaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "11",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magnaderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     rowId: "12",
-    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco latate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 ];
 
 export const streamFromServer = server$(async function* (
   // Rows should comes from the db.
-  rows: Row[],
+  row: Row,
   column: Column,
 ) {
-  for (const row of rows) {
-    const response = responses.find((r) => r.rowId === row.id)!;
+  const response = responses.find((r) => r.rowId === row.id)!;
 
-    row.data[column.name].generating = false;
-    for (const letters of response.data) {
-      row.data[column.name].value += letters;
+  row.data[column.name].generating = false;
 
-      yield row;
+  for (const letters of response.data) {
+    row.data[column.name].value += letters;
 
-      await new Promise((resolve) => setTimeout(resolve, 15));
-    }
+    yield row;
 
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await new Promise((resolve) => setTimeout(resolve, 15));
   }
 });
 
@@ -80,6 +77,7 @@ export const useHome = () => {
 
   const onCreateColumn = $(async (newColum: Column) => {
     columns.value = [...columns.value, newColum];
+
     rows.value = rows.value.map((row) => ({
       ...row,
       data: {
@@ -91,12 +89,20 @@ export const useHome = () => {
       },
     }));
 
-    const response = await streamFromServer(rows.value, newColum);
+    const stream = async (row: Row) => {
+      const response = await streamFromServer(row, newColum);
 
-    for await (const value of response) {
-      rows.value = rows.value.map((row) => {
-        return row.id === value.id ? value : row;
-      });
+      for await (const value of response) {
+        rows.value = rows.value.map((row) => {
+          return row.id === value.id ? value : row;
+        });
+      }
+    };
+
+    for (const row of rows.value) {
+      stream(row);
+
+      await new Promise((resolve) => setTimeout(resolve, 100));
     }
   });
 
