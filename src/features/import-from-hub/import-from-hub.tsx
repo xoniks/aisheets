873fb50c08763d4064019e2ef0c6a9d4af027faa
Paco Aranda <francisco.aranda@huggingface.co>
2025-03-04 15:34:32 +0100

    Improve import form (#97)
    * chore: Improve import from hub form logic
    
    * Adapt import form + datasets search
    
    * Refactor layout of ImportFromHub component for improved styling and structure
    
    ---------
    
    Co-authored-by: Damián Pumar <damianpumar@gmail.com>

--- src/features/import-from-hub/import-from-hub.tsx
+++ src/features/import-from-hub/import-from-hub.tsx
@@ -7,12 +7,14 @@ import {
   useResource$,
   useSignal,
   useTask$,
+  useVisibleTask$,
 } from '@builder.io/qwik';
 import { useNavigate } from '@builder.io/qwik-city';
 import { LuCheck, LuLoader, LuTerminalSquare } from '@qwikest/icons/lucide';
 
 import { Button, Input, Label, Select } from '~/components';
 import { useSession } from '~/loaders';
+import { listDatasets } from '~/services/repository/hub/list-datasets';
 import { listHubDatasetDataFiles } from '~/services/repository/hub/list-hub-dataset-files';
 import { useImportFromHub } from '~/usecases/import-from-hub.usecase';
 
@@ -21,11 +23,16 @@ export const ImportFromHub = component$(() => {
   const importFromHub = useImportFromHub();
   const nav = useNavigate();
 
-  const showFileSelection = useSignal(false);
   const isImportingData = useSignal(false);
 
   const repoId = useSignal<string | undefined>(undefined);
   const filePath = useSignal<string | undefined>(undefined);
+
+  useVisibleTask$(({ track }) => {
+    track(repoId);
+
+    filePath.value = undefined;
+  });
 
   const handleOnClickImportFromHub = $(async () => {
     try {
@@ -48,52 +55,38 @@ export const ImportFromHub = component$(() => {
   });
 
   return (
-    <div class="flex flex-col justify-between gap-4">
-      <h1 class="text-3xl font-bold w-full">Import you dataset from the hub</h1>
+    <div class="flex flex-col w-fit mt-8 gap-4">
+      <div class="flex flex-col justify-between gap-4">
+        <h1 class="text-3xl font-bold w-full">
+          Import your dataset from the hub
+        </h1>
 
-      <div class="flex flex-col gap-4">
-        <div class="flex items-center justify-between">
-          <Label for="dataset-repo-id">Repo id</Label>
-        </div>
-        <div class="flex w-full max-w-sm items-center space-x-2">
-          <Input
-            id="dataset-repo-id"
-            class="h-10"
-            placeholder="Enter the repo id"
-            bind:value={repoId}
-            onChange$={(e) => {
-              showFileSelection.value = false;
-              filePath.value = undefined;
-            }}
-          />
-          <Button
-            id="explore-dataset"
-            size="sm"
-            class="h-10 rounded-sm"
-            onClick$={() => {
-              showFileSelection.value = true;
-            }}
-          >
-            Explore files
-          </Button>
-        </div>
-
-        {showFileSelection.value ? (
-          <div>
-            <FileSelection
-              repoId={repoId.value!}
-              accessToken={session.value!.token}
-              onSelectedFile$={(file) => {
-                filePath.value = file;
+        <div class="flex flex-row gap-4">
+          <div class="flex-1 max-w-xl">
+            <DatasetSearch
+              onSelectedDataset$={(dataset) => {
+                repoId.value = dataset;
               }}
             />
           </div>
-        ) : (
-          <div />
-        )}
+
+          {repoId.value ? (
+            <div class="flex-1 max-w-md">
+              <FileSelection
+                repoId={repoId.value!}
+                accessToken={session.value!.token}
+                onSelectedFile$={(file) => {
+                  filePath.value = file;
+                }}
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
 
-      <div class="w-full flex flex-col gap-2">
+      <div class="flex flex-col w-full gap-4 items-end">
         <Button
           look="primary"
           disabled={!enableImportButton.value}
@@ -107,16 +100,93 @@ export const ImportFromHub = component$(() => {
           ) : (
             <div class="flex items-center gap-4">
               <LuTerminalSquare class="text-xl" />
-
               <span>Import dataset</span>
             </div>
           )}
         </Button>
-        <p class="text-sm">Only the first 1000 rows will be imported.</p>
+        {repoId.value && filePath.value && (
+          <div class="text-foreground text-sm">
+            <span>Only the first 1000 rows will be imported</span>
+          </div>
+        )}
       </div>
     </div>
   );
 });
+
+const DatasetSearch = component$(
+  (props: {
+    onSelectedDataset$: QRL<(dataset: string) => void>;
+  }) => {
+    const session = useSession();
+    const searchQuery = useSignal('');
+    const selectedDataset = useSignal<string | undefined>(undefined);
+
+    const searchResults = useResource$(async ({ track }) => {
+      track(searchQuery);
+      if (searchQuery.value.trim() === '') return [];
+      return (
+        await listDatasets({
+          query: searchQuery.value,
+          accessToken: session.value!.token,
+          limit: 5,
+        })
+      ).map((dataset) => dataset.name);
+    });
+
+    useTask$(({ track }) => {
+      track(selectedDataset);
+      if (selectedDataset.value) {
+        searchQuery.value = selectedDataset.value;
+        props.onSelectedDataset$(selectedDataset.value);
+      }
+    });
+
+    return (
+      <div class="flex flex-col gap-4">
+        <Label for="dataset-search">Dataset id</Label>
+        <Input
+          id="dataset-search"
+          class="h-10"
+          placeholder="Type to search datasets"
+          bind:value={searchQuery}
+        />
+        <Resource
+          value={searchResults}
+          onRejected={(error) => {
+            console.error(error);
+            return (
+              <div class="flex items-center justify-center h-32 background-primary rounded-base">
+                <span class="text-foreground warning">
+                  Failed to fetch datasets. Please, try again.
+                </span>
+              </div>
+            );
+          }}
+          onResolved={(datasets) => (
+            <div class="flex flex-col gap-2">
+              {searchQuery.value.trim() !== '' && datasets.length === 0 ? (
+                <></>
+              ) : (
+                datasets.map((dataset, idx) => (
+                  <Button
+                    key={idx}
+                    class="text-left"
+                    onClick$={() => {
+                      selectedDataset.value = dataset;
+                    }}
+                  >
+                    {dataset}
+                  </Button>
+                ))
+              )}
+            </div>
+          )}
+        />
+      </div>
+    );
+  },
+);
 
 const FileSelection = component$(
   (props: {
@@ -124,14 +194,21 @@ const FileSelection = component$(
     accessToken: string;
     onSelectedFile$: QRL<(file: string) => void>;
   }) => {
-    const listDatasetFiles = useResource$(async () => {
-      return await listHubDatasetDataFiles({
-        repoId: props.repoId,
+    const selectedFile = useSignal<string>('');
+
+    const listDatasetFiles = useResource$(async ({ track }) => {
+      const newRepo = track(() => props.repoId);
+
+      const files = await listHubDatasetDataFiles({
+        repoId: newRepo,
         accessToken: props.accessToken,
       });
-    });
 
-    const selectedFile = useSignal<string | undefined>(undefined);
+      if (files.length === 0) selectedFile.value = '';
+      else selectedFile.value = files[0];
+
+      return files;
+    });
 
     useTask$(({ track }) => {
       const newValue = track(selectedFile);
@@ -153,34 +230,39 @@ const FileSelection = component$(
           );
         }}
         onResolved={(files) => {
-          if (!selectedFile.value) selectedFile.value = files[0];
-
           return (
             <div class="flex flex-col gap-4">
-              <Label for="dataset-file">Select a file to import</Label>
+              <Label for="dataset-file">File</Label>
 
-              <Select.Root value={selectedFile.value} class="relative">
-                <Select.Trigger class="px-4 bg-primary rounded-base border-secondary-foreground">
-                  <Select.DisplayValue />
-                </Select.Trigger>
-                <Select.Popover class="bg-primary border border-border max-h-[300px] overflow-y-auto top-[100%] bottom-auto">
-                  {files.map((file, idx) => (
-                    <Select.Item
-                      key={idx}
-                      class="text-foreground hover:bg-accent"
-                      value={file}
-                      onClick$={() => {
-                        selectedFile.value = file;
-                      }}
-                    >
-                      <Select.ItemLabel>{file}</Select.ItemLabel>
-                      <Select.ItemIndicator>
-                        <LuCheck class="h-4 w-4" />
-                      </Select.ItemIndicator>
-                    </Select.Item>
-                  ))}
-                </Select.Popover>
-              </Select.Root>
+              {files.length === 0 ? (
+                <span class="text-foreground warning">
+                  No compatible files found in this dataset. Only jsonl, csv,
+                  and parquet files are supported.
+                </span>
+              ) : (
+                <Select.Root bind:value={selectedFile} class="relative">
+                  <Select.Trigger class="px-4 bg-primary rounded-base border-secondary-foreground">
+                    <Select.DisplayValue />
+                  </Select.Trigger>
+                  <Select.Popover class="bg-primary border border-border max-h-[300px] overflow-y-auto top-[100%] bottom-auto">
+                    {files.map((file, idx) => (
+                      <Select.Item
+                        key={idx}
+                        class="text-foreground hover:bg-accent"
+                        value={file}
+                        onClick$={() => {
+                          selectedFile.value = file;
+                        }}
+                      >
+                        <Select.ItemLabel>{file}</Select.ItemLabel>
+                        <Select.ItemIndicator>
+                          <LuCheck class="h-4 w-4" />
+                        </Select.ItemIndicator>
+                      </Select.Item>
+                    ))}
+                  </Select.Popover>
+                </Select.Root>
+              )}
             </div>
           );
         }}
--- src/services/repository/hub/list-datasets.ts
+++ src/services/repository/hub/list-datasets.ts
@@ -4,14 +4,17 @@ import * as hub from '@huggingface/hub';
 export const listDatasets = async ({
   accessToken,
   query,
+  limit,
 }: {
   accessToken: string;
   query?: string;
+  limit?: number;
 }): Promise<DatasetEntry[]> => {
   const hubDatasets = [];
   for await (const dataset of hub.listDatasets({
     accessToken,
     search: query ? { query: query } : undefined,
+    limit,
   })) {
     hubDatasets.push(dataset);
   }
