6497a2cee75b7b59b6a4f8e592b7fa36a86a63f4
Paco Aranda <francisco.aranda@huggingface.co>
2025-06-05 10:25:25 +0200

    [BUGFIX] Robust rows serialization (#333)
    * fix: Using new JS serializers
    
    * update patch version for duckdb

--- src/services/repository/tables/list-table-rows.ts
+++ src/services/repository/tables/list-table-rows.ts
@@ -4,12 +4,6 @@ import { getColumnName, getDatasetTableName } from './utils';
 import fs from 'node:fs/promises';
 import os from 'node:os';
 import path from 'node:path';
-import {
-  DuckDBBlobValue,
-  DuckDBListValue,
-  DuckDBMapValue,
-  DuckDBStructValue,
-} from '@duckdb/node-api';
 
 export const countDatasetTableRows = async ({
   dataset,
@@ -79,62 +73,9 @@ export const listDatasetTableRows = async ({
     }
 
     const results = await db.run(statement);
+    const rows = await results.getRowObjectsJS();
 
-    const rows = await results.getRowObjects();
-
-    const cleanValues = (row: any): any => {
-      if (row instanceof DuckDBListValue) {
-        row = row.items;
-
-        for (let i = 0; i < row.length; i++) {
-          row[i] = cleanValues(row[i]);
-        }
-
-        return row;
-      }
-
-      if (row instanceof DuckDBStructValue || row instanceof DuckDBMapValue) {
-        row = row.entries;
-        return cleanValues(row);
-      }
-
-      for (const key in row) {
-        let value = row[key];
-
-        if (value instanceof DuckDBListValue) {
-          row[key] = row[key].items;
-
-          for (let i = 0; i < row[key].length; i++) {
-            row[key][i] = cleanValues(row[key][i]);
-          }
-        }
-
-        if (
-          value instanceof DuckDBStructValue ||
-          row instanceof DuckDBMapValue
-        ) {
-          value = value.entries;
-          cleanValues(value);
-          row[key] = value;
-        }
-
-        if (value instanceof DuckDBBlobValue) {
-          row[key] = row[key].bytes;
-        }
-
-        try {
-          JSON.stringify(row[key]);
-        } catch (e) {
-          row[key] = row[key].toString();
-        }
-      }
-
-      return row;
-    };
-
-    const cleanedRows = rows.map(cleanValues);
-
-    return cleanedRows;
+    return rows;
   });
 };
 
